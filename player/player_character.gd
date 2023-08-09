9edea516c46c397c1274fbc33090706f15834241
randomnewb <nguy1581@gmail.com>
2023-08-09 13:59:44 -0500

    fix animation when using mouse, update readme

--- player/player_character.gd
+++ player/player_character.gd
@@ -15,11 +15,11 @@ extends CharacterBody2D
 
 
 func _process(delta):
-	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down").normalized()
+	
 	
 	if Input.is_action_pressed("tap_left_mouse"):
-		var target = get_local_mouse_position();
-		target /= position;
+		var target = get_global_mouse_position() - global_position;
 		input_vector = target.normalized();
 
 	returned_direction(input_vector)
@@ -31,10 +31,8 @@ func _process(delta):
 	else:
 		animation = "idle_" + str(last_direction)
 		animated_sprite_2d.play(animation);
-#		animation_player.play("walk_down");
 	position.x = clamp(position.x, 5, width - 5);
 	position.y = clamp(position.y, 5, height - 5);
-	#position += input_vector * speed * delta;
 	
 	#ray_cast code
 #	ray_cast_2d.target_position = input_vector * 25;
@@ -44,15 +42,15 @@ func _process(delta):
 	
 	move_and_collide(input_vector * speed * delta);
 
-func returned_direction(input_vector: Vector2):
-	var normalized = input_vector
-	if normalized.y <= -1.0:
-		return "up";
-	elif normalized.y >= 1.0:
-		return "down";
-	elif normalized.x >= 1.0:
-		return "right"
-	elif normalized.x <= -1.0:
-		return "left"
-		
-	return "";
+func returned_direction(vector: Vector2):
+	vector.normalized();
+	if abs(vector.x) > abs(vector.y):
+		if vector.x < 0.0:
+			return "left"
+		else:
+			return "right"
+	else:
+		if vector.y < 0.0:
+			return "up";
+		else:
+			return "down";
